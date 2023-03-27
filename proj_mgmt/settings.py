fda287bdb5911b5dbd17855db876baf1ddd1c493
Santosh Vishwanathan <73582195+Santoshvish697@users.noreply.github.com>
2023-03-27 20:19:33 +0530

    deploy_try_1

--- proj_mgmt/settings.py
+++ proj_mgmt/settings.py
@@ -33,7 +33,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['santoshvish.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
